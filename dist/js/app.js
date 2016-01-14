43afbb39cc55919794998e13af4a9b1080d40788
Kiran <kiranbose@gmail.com>
2016-01-15 01:18:02 +0530

    final build- GULP boilerplate code

---
+++ dist/js/app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
