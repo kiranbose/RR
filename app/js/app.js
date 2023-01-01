5c48394e5950a209ce6209e6454f1020bd0f6424
Charly Abraham <charly@core.ai>
2023-01-01 17:48:18 +0530

    poc for intial website

--- app/js/app.js
+++ app/js/app.js
@@ -248,6 +248,130 @@ $(document).ready(function() {
 
 (function (window, angular, undefined) {
     "use strict";
+    var rrFestivalsControllers = angular.module('rr.gallery.controllers', []);
+
+    rrFestivalsControllers.controller('galleryController', ['$scope','$window',function ($scope,$window) {
+        $('html, body').animate({scrollTop: 0}, "slow");
+    //angular.element(document.querySelector( '#selectedMenu' )).html('EVENTS');
+    $scope.selectedMenuItem = "EVENTS";
+    $scope.pageData = "Hello event";
+    console.log("hello");
+
+        var $images = $('.docs-pictures');
+        var $toggles = $('.docs-toggles');
+        var $buttons = $('.docs-buttons');
+        var options = {
+            // inline: true,
+            rotatable: false,
+            url: 'data-original',
+
+        };
+
+        function toggleButtons(mode) {
+            if (/modal|inline|none/.test(mode)) {
+                $buttons.
+                find('button[data-enable]').
+                prop('disabled', true).
+                filter('[data-enable*="' + mode + '"]').
+                prop('disabled', false);
+            }
+        }
+
+        $images.viewer(options);
+
+        toggleButtons(options.inline ? 'inline' : 'modal');
+
+        $toggles.on('change', 'input', function () {
+            var $input = $(this);
+            var name = $input.attr('name');
+
+            options[name] = name === 'inline' ? $input.data('value') : $input.prop('checked');
+            $images.viewer('destroy').viewer(options);
+            toggleButtons(options.inline ? 'inline' : 'modal');
+        });
+
+        $scope.galleryImages = [{imgsrc:'images/gallery/1.jpg',heading:'fsdfd'},
+            {imgsrc:'images/gallery/2.jpg',heading:'fffsafsd'},
+            {imgsrc:'images/gallery/3.jpg',heading:'fdsf'},
+            {imgsrc:'images/gallery/4.jpg',heading:'fdsf'},
+            {imgsrc:'images/gallery/20.jpg',heading:'fsfas'}];
+
+
+        $scope.download= function(file){
+            //window.location.href = $scope.downloadLink;
+            //window.open($scope.downloadLink);
+            var link = document.createElement("a");
+            link.download = file;
+            link.href = file;
+            link.click();
+        };
+    }]);
+
+})(window, window.angular);
+
+/**
+ * Created by kiran on 1/17/2016.
+ */
+(function(window, angular, undefined) {
+    "use strict";
+
+    angular.module("rr.gallery", [
+        "rr.gallery.controllers"
+    ]);
+
+})(window, window.angular);
+
+
+/**
+ * Created by kiran on 1/17/2016.
+ */
+
+(function (window, angular, undefined) {
+    "use strict";
+    var rrHomeControllers = angular.module('rr.home.controllers', []);
+
+    rrHomeControllers.controller('homeController', ['$scope','$window',function ($scope,$window) {
+        $('html, body').animate({scrollTop: 0}, "slow");
+
+        $('.carousel').carousel({
+            interval: 3000
+        });
+        //angular.element(document.querySelector( '#selectedMenu' )).html('EVENTS');
+        $scope.selectedMenuItem = "HOME";
+        $scope.pageData = "Hello HOME";
+        console.log("Howdy.. geek. \n Drop a mail to kiranbose@gmail.com for any queries. \n May the Force be with you !");
+
+        $scope.upaDevatas = [{Name:'Muthappan',imageSrc:'images/round/r1.gif',text:''},
+            {Name:'Yogeeswaran',imageSrc:'images/round/r2.jpg',text:''},
+            {Name:'NagaRaja',imageSrc:'images/round/r3.jpg',text:''},
+            {Name:'Naga YakshiAmma',imageSrc:'images/round/r4.jpg',text:''},
+            {Name:'Naga Kanyaka',imageSrc:'images/round/r5.jpg',text:''},
+            {Name:'Bhrahmarakshass',imageSrc:'images/round/r5.jpg',text:''},
+           ];
+
+        //    enable this during ulsavam time to display and update popup
+        //    $(".ulsavamBtn").trigger('click');
+    }]);
+
+
+})(window, window.angular);
+/**
+ * Created by kiran on 1/17/2016.
+ */
+(function(window, angular, undefined) {
+    "use strict";
+
+    angular.module("rr.home", [
+        "rr.home.controllers"
+    ]);
+
+})(window, window.angular);
+/**
+ * Created by kiran on 1/17/2016.
+ */
+
+(function (window, angular, undefined) {
+    "use strict";
     var rrPoojasControllers = angular.module('rr.poojas.controllers', []);
 
     rrPoojasControllers.controller('poojasController', ['$scope', '$window', function ($scope, $window) {
@@ -325,130 +449,6 @@ $(document).ready(function() {
     ]);
 
 })(window, window.angular);
-/**
- * Created by kiran on 1/17/2016.
- */
-
-(function (window, angular, undefined) {
-    "use strict";
-    var rrHomeControllers = angular.module('rr.home.controllers', []);
-
-    rrHomeControllers.controller('homeController', ['$scope','$window',function ($scope,$window) {
-        $('html, body').animate({scrollTop: 0}, "slow");
-
-        $('.carousel').carousel({
-            interval: 3000
-        });
-        //angular.element(document.querySelector( '#selectedMenu' )).html('EVENTS');
-        $scope.selectedMenuItem = "HOME";
-        $scope.pageData = "Hello HOME";
-        console.log("Howdy.. geek. \n Drop a mail to kiranbose@gmail.com for any queries. \n May the Force be with you !");
-
-        $scope.upaDevatas = [{Name:'Muthappan',imageSrc:'images/round/r1.gif',text:''},
-            {Name:'Yogeeswaran',imageSrc:'images/round/r2.jpg',text:''},
-            {Name:'NagaRaja',imageSrc:'images/round/r3.jpg',text:''},
-            {Name:'Naga YakshiAmma',imageSrc:'images/round/r4.jpg',text:''},
-            {Name:'Naga Kanyaka',imageSrc:'images/round/r5.jpg',text:''},
-            {Name:'Bhrahmarakshass',imageSrc:'images/round/r5.jpg',text:''},
-           ];
-
-        //    enable this during ulsavam time to display and update popup
-        //    $(".ulsavamBtn").trigger('click');
-    }]);
-
-
-})(window, window.angular);
-/**
- * Created by kiran on 1/17/2016.
- */
-(function(window, angular, undefined) {
-    "use strict";
-
-    angular.module("rr.home", [
-        "rr.home.controllers"
-    ]);
-
-})(window, window.angular);
-/**
- * Created by kiran on 1/17/2016.
- */
-
-(function (window, angular, undefined) {
-    "use strict";
-    var rrFestivalsControllers = angular.module('rr.gallery.controllers', []);
-
-    rrFestivalsControllers.controller('galleryController', ['$scope','$window',function ($scope,$window) {
-        $('html, body').animate({scrollTop: 0}, "slow");
-    //angular.element(document.querySelector( '#selectedMenu' )).html('EVENTS');
-    $scope.selectedMenuItem = "EVENTS";
-    $scope.pageData = "Hello event";
-    console.log("hello");
-
-        var $images = $('.docs-pictures');
-        var $toggles = $('.docs-toggles');
-        var $buttons = $('.docs-buttons');
-        var options = {
-            // inline: true,
-            rotatable: false,
-            url: 'data-original',
-
-        };
-
-        function toggleButtons(mode) {
-            if (/modal|inline|none/.test(mode)) {
-                $buttons.
-                find('button[data-enable]').
-                prop('disabled', true).
-                filter('[data-enable*="' + mode + '"]').
-                prop('disabled', false);
-            }
-        }
-
-        $images.viewer(options);
-
-        toggleButtons(options.inline ? 'inline' : 'modal');
-
-        $toggles.on('change', 'input', function () {
-            var $input = $(this);
-            var name = $input.attr('name');
-
-            options[name] = name === 'inline' ? $input.data('value') : $input.prop('checked');
-            $images.viewer('destroy').viewer(options);
-            toggleButtons(options.inline ? 'inline' : 'modal');
-        });
-
-        $scope.galleryImages = [{imgsrc:'images/gallery/1.jpg',heading:'fsdfd'},
-            {imgsrc:'images/gallery/2.jpg',heading:'fffsafsd'},
-            {imgsrc:'images/gallery/3.jpg',heading:'fdsf'},
-            {imgsrc:'images/gallery/4.jpg',heading:'fdsf'},
-            {imgsrc:'images/gallery/20.jpg',heading:'fsfas'}];
-
-
-        $scope.download= function(file){
-            //window.location.href = $scope.downloadLink;
-            //window.open($scope.downloadLink);
-            var link = document.createElement("a");
-            link.download = file;
-            link.href = file;
-            link.click();
-        };
-    }]);
-
-})(window, window.angular);
-
-/**
- * Created by kiran on 1/17/2016.
- */
-(function(window, angular, undefined) {
-    "use strict";
-
-    angular.module("rr.gallery", [
-        "rr.gallery.controllers"
-    ]);
-
-})(window, window.angular);
-
-
 /**
  * Created by kiran on 1/17/2016.
  */
